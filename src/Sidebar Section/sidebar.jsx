2b49881846d3b1202eecbea543e1e6cf400df646
rirefat <rirefat.official@gmail.com>
2022-12-23 03:22:07 +0600

    done upto icon importing

--- src/Sidebar Section/sidebar.jsx
+++ src/Sidebar Section/sidebar.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
 import './sidebar.css';
+import logo from '../assets/logo.png'
+import { BsSpeedometer } from "react-icons/bs";
 
 const sidebar = () => {
     return (
-        <div>
-            <h1>Sidebar</h1>
+        <div className='sideBar grid'>
+            <div className="logoDiv flex">
+                <img src={logo} alt="" />
+                <h2>Planti.</h2>
+            </div>
+
+            <div className="menuDiv">
+                <h3 className="divTitle">QUICK MENU</h3>
+                <ul className="menuLists grid">
+                    <li className="listItem">
+                        <a href="#" className="menuLink flex"><BsSpeedometer className='icon'/></a>
+                    </li>
+                </ul>
+            </div>
         </div>
     );
 };
