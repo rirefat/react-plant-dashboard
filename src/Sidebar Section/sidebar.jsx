8f34ce3f8cbabaf8c79652e17cbb4519576ac084
rirefat <rirefat.official@gmail.com>
2022-12-23 03:30:29 +0600

    done sidebar main div

--- src/Sidebar Section/sidebar.jsx
+++ src/Sidebar Section/sidebar.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import './sidebar.css';
 import logo from '../assets/logo.png'
-import { BsSpeedometer } from "react-icons/bs";
+import { BsSpeedometer, BsTrophy } from "react-icons/bs";
+import { MdDeliveryDining, MdOutlineExplore } from "react-icons/md";
 
 const sidebar = () => {
     return (
@@ -15,7 +16,30 @@ const sidebar = () => {
                 <h3 className="divTitle">QUICK MENU</h3>
                 <ul className="menuLists grid">
                     <li className="listItem">
-                        <a href="#" className="menuLink flex"><BsSpeedometer className='icon'/></a>
+                        <a href="#" className="menuLink flex">
+                            <BsSpeedometer className='icon'/>
+                            <span className="smallText">Dash Board</span>
+                        </a>                        
+                    </li>
+                    
+                    <li className="listItem">
+                        <a href="#" className="menuLink flex">
+                            <MdDeliveryDining className='icon'/>
+                            <span className="smallText">My Orders</span>
+                        </a>
+                    </li>
+                    
+                    <li className="listItem">
+                        <a href="#" className="menuLink flex">
+                            <MdOutlineExplore className='icon'/>
+                            <span className="smallText">Explore</span>
+                        </a>
+                    </li>
+                    <li className="listItem">
+                        <a href="#" className="menuLink flex">
+                            <BsTrophy className='icon'/>
+                            <span className="smallText">Products</span>
+                        </a>
                     </li>
                 </ul>
             </div>
