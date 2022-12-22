df64ab4b5e05d7dda386e1d7d74c37de9f8c0fcc
rirefat <rirefat.official@gmail.com>
2022-12-23 03:40:37 +0600

    done upto sidebar settings section

--- src/Sidebar Section/sidebar.jsx
+++ src/Sidebar Section/sidebar.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import './sidebar.css';
 import logo from '../assets/logo.png'
-import { BsSpeedometer, BsTrophy } from "react-icons/bs";
-import { MdDeliveryDining, MdOutlineExplore } from "react-icons/md";
+import { BsSpeedometer, BsTrophy,BsCreditCard2Front } from "react-icons/bs";
+import { AiOutlinePieChart } from "react-icons/ai";
+import { BiTrendingUp } from "react-icons/bi";
+import { MdDeliveryDining, MdOutlineExplore,MdOutlinePermContactCalendar } from "react-icons/md";
 
 const sidebar = () => {
     return (
@@ -12,6 +14,7 @@ const sidebar = () => {
                 <h2>Planti.</h2>
             </div>
 
+            {/*========================================== Main Sidebar ==========================================*/}
             <div className="menuDiv">
                 <h3 className="divTitle">QUICK MENU</h3>
                 <ul className="menuLists grid">
@@ -39,6 +42,39 @@ const sidebar = () => {
                         <a href="#" className="menuLink flex">
                             <BsTrophy className='icon'/>
                             <span className="smallText">Products</span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+
+            {/*========================================== Sidebar Settings ==========================================*/}
+            <div className="settings">
+                <h3 className="divTitle">SETTINGS</h3>
+                <ul className="menuLists grid">
+                    <li className="listItem">
+                        <a href="#" className="menuLink flex">
+                            <AiOutlinePieChart className='icon'/>
+                            <span className="smallText">Charts</span>
+                        </a>                        
+                    </li>
+                    
+                    <li className="listItem">
+                        <a href="#" className="menuLink flex">
+                            <BiTrendingUp className='icon'/>
+                            <span className="smallText">Trendings</span>
+                        </a>
+                    </li>
+                    
+                    <li className="listItem">
+                        <a href="#" className="menuLink flex">
+                            <MdOutlinePermContactCalendar className='icon'/>
+                            <span className="smallText">Contract</span>
+                        </a>
+                    </li>
+                    <li className="listItem">
+                        <a href="#" className="menuLink flex">
+                            <BsCreditCard2Front className='icon'/>
+                            <span className="smallText">Billing</span>
                         </a>
                     </li>
                 </ul>
