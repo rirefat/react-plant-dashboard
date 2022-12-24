f6c1c1db2b9f4b3e10924c46ca12306a92a6aa10
rirefat <rirefat.official@gmail.com>
2022-12-25 02:26:27 +0600

    done upto listing secContainer

--- src/Body Section/Listing Section/listing.jsx
+++ src/Body Section/Listing Section/listing.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
 import './listing.css';
 
+// Imported Icons
+import { BsArrowRightShort,BsHeartFill } from 'react-icons/bs';
+
 const listing = () => {
     return (
-        <div>
-            listing
+        <div className="listingSection">
+            <div className="heading flex">
+                <h1>My Listings</h1>
+                <button className="btn flex">
+                    See All <BsArrowRightShort className='icon'></BsArrowRightShort>
+                </button>
+            </div>
+
+            <div className="secContainer flex">
+                <div className="singleItem">
+                    <BsHeartFill className='icon'></BsHeartFill>
+                    <img src="" alt="" />
+                </div>
+            </div>
         </div>
     );
 };
