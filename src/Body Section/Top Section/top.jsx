e08f811b087bdbd14e391c39de0f66f55dfaef49
rirefat <rirefat.official@gmail.com>
2022-12-25 01:52:41 +0600

    done upto top right section

--- src/Body Section/Top Section/top.jsx
+++ src/Body Section/Top Section/top.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './top.css';
-import img from '../../assets/rafiul_refat.jpg'
-import video from '../../assets/plant.mp4'
+import img from '../../assets/rafiul_refat.jpg';
+import tree from '../../assets/tree.jpg';
+import video from '../../assets/plant.mp4';
 
 // Imported Icons
-import { BsSearch } from 'react-icons/bs';
+import { BsSearch, BsArrowReturnRight,BsQuestionCircle } from 'react-icons/bs';
 import { TbMessageCircle } from 'react-icons/tb';
 import { IoMdNotificationsOutline } from 'react-icons/io';
 
@@ -19,7 +20,7 @@ const top = () => {
                 </div>
 
                 <div className="searchBar flex">
-                    <input type="text" name="" id="" placeholder='Search Dashboard'/>
+                    <input type="text" name="" id="" placeholder='Search Dashboard' />
                     <BsSearch className='icon'></BsSearch>
                 </div>
 
@@ -45,6 +46,44 @@ const top = () => {
 
                     <div className="videoDiv">
                         <video src={video} autoPlay loop muted></video>
+                    </div>
+                </div>
+
+                <div className="leftCard flex">
+                    <div className="main flex">
+                        <div className="textDiv">
+                            <h1>My Stat</h1>
+
+                            <div className="flex">
+                                <span>
+                                    Today <br /> <small>4 Orders</small>
+                                </span>
+                                <span>
+                                    This Month <br /> <small>124 Orders</small>
+                                </span>
+                            </div>
+
+                            <span className="flex link">
+                                Go to my orders<BsArrowReturnRight className='icon'></BsArrowReturnRight>
+                            </span>
+
+                        </div>
+
+                        <div className="imgDiv">
+                            <img src={tree} alt="User img" />
+                        </div>
+
+                        {/* <div className="sideBarCard">
+                            <BsQuestionCircle className='icon'></BsQuestionCircle>
+                            <div className="cardContent">
+                                <div className="circle1"></div>
+                                <div className="circle2"></div>
+
+                                <h3>Help Center</h3>
+                                <p>Having trouble in Planti, please contact us from for more questions.</p>
+                                <button className="btn">Go to help center</button>
+                            </div>
+                        </div> */}
                     </div>
                 </div>
             </div>
