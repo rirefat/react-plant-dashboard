7fd0ec9a85f72c06425a5d68fdd54172e94612fb
rirefat <rirefat.official@gmail.com>
2022-12-25 05:40:59 +0600

    done upto single user

--- src/Body Section/Listing Section/listing.jsx
+++ src/Body Section/Listing Section/listing.jsx
@@ -4,9 +4,10 @@ import img from '../../assets/plant2.png';
 import img1 from '../../assets/plant1.png';
 import img4 from '../../assets/plant4.png';
 import img3 from '../../assets/plant6.png';
+import user1 from '../../assets/user1.jpg';
 
 // Imported Icons
-import { BsArrowRightShort,BsHeartFill,BsHeart } from 'react-icons/bs';
+import { BsArrowRightShort, BsHeartFill, BsHeart } from 'react-icons/bs';
 
 const listing = () => {
     return (
@@ -38,6 +39,26 @@ const listing = () => {
                     <BsHeartFill className='icon'></BsHeartFill>
                     <img src={img3} alt="" />
                     <h3>Spider Plant</h3>
+                </div>
+            </div>
+
+            <div className="sellers flex">
+                <div className="topSellers">
+                    <div className="heading flex">
+                        <h3>Top Sellers</h3>
+                        <button className="btn flex">
+                            See All <BsArrowRightShort className='icon'></BsArrowRightShort>
+                        </button>
+                    </div>
+
+                    <div className="card flex">
+                        <div className="users">
+                            <img src={user1} alt="" />
+                        </div>
+                        <div className="cardText">
+                            <span>14.556 Plants Sold</span>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
