f14340b9d038f3caefc635001007f60324e7aabe
rirefat <rirefat.official@gmail.com>
2022-12-25 00:26:19 +0600

    done upto top video

--- src/Body Section/Top Section/top.jsx
+++ src/Body Section/Top Section/top.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './top.css';
 import img from '../../assets/rafiul_refat.jpg'
+import video from '../../assets/plant.mp4'
 
 // Imported Icons
 import { BsSearch } from 'react-icons/bs';
@@ -10,6 +11,7 @@ import { IoMdNotificationsOutline } from 'react-icons/io';
 const top = () => {
     return (
         <div className="topSection">
+            {/*=================================== Header Section ===================================*/}
             <div className="headerSection flex">
                 <div className="title">
                     <h1>Welcome to Planti.</h1>
@@ -26,6 +28,23 @@ const top = () => {
                     <IoMdNotificationsOutline className='icon'></IoMdNotificationsOutline>
                     <div className="adminImage">
                         <img src={img} alt="" />
+                    </div>
+                </div>
+            </div>
+
+            {/*=================================== Card Section ====================================*/}
+            <div className="cardSection flex">
+                <div className="rightCard flex">
+                    <h1>Create and sell extraordinary products</h1>
+                    <p>The world's first growing industry today are natural made products!</p>
+
+                    <div className="buttons flex">
+                        <button className="btn">Explore More</button>
+                        <button className="btn transparent">Top Sellers</button>
+                    </div>
+
+                    <div className="videoDiv">
+                        <video src={video} autoPlay loop muted></video>
                     </div>
                 </div>
             </div>
